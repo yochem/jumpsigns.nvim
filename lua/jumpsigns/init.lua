7b98cc0d5063d73403c63f7b93cfa86296bf8ef3
Yochem van Rosmalen <git@yochem.nl>
2024-11-20 18:56:08 +0100

    fix: scroll past last line bug

--- lua/jumpsigns/init.lua
+++ lua/jumpsigns/init.lua
@@ -1,73 +1,90 @@
 local M = {}
 
-local ns = vim.api.nvim_create_namespace("jumpsigns")
+M.config = require("jumpsigns.config")
+
+local ns = vim.api.nvim_create_namespace("JumpSigns")
 
 local function hml_signs()
-  local last_line = vim.api.nvim_buf_line_count(0)
-  if last_line <= 3 then
+  local viewport_top = vim.fn.line('w0')
+  local viewport_bottom = vim.fn.line('w$')
+  if viewport_bottom - viewport_top < 2 then
     return {}
   end
+  local last_line = vim.api.nvim_buf_line_count(0)
 
   local scrolloff = vim.wo.scrolloff
 
-  -- No offset when first line is visible
-  local viewport_top = vim.fn.line('w0')
-  local H_offset = viewport_top == 1 and 0 or scrolloff
-  local H_linenr = viewport_top + H_offset
+  local H_linenr
+  if viewport_top == 1 then
+    H_linenr = 1
+  elseif viewport_top + scrolloff < viewport_bottom then
+    H_linenr = viewport_top + scrolloff
+  end
 
-  -- No offset when last line is visible
-  local viewport_bottom = vim.fn.line('w$')
-  local L_offset = viewport_bottom >= last_line and 0 or scrolloff
-  local L_linenr = viewport_bottom - L_offset
+  local L_linenr
+  if viewport_bottom >= last_line then
+    L_linenr = last_line
+  elseif viewport_bottom - scrolloff >= viewport_top then
+    L_linenr = viewport_bottom - scrolloff
+  end
 
-  local M_linenr = math.floor((viewport_bottom - viewport_top) / 2 + viewport_top)
+  local M_linenr
+  local middle = math.floor((viewport_bottom - viewport_top) / 2 + viewport_top)
+  if middle < viewport_bottom - scrolloff then
+    M_linenr = middle
+  end
 
   return { H = H_linenr, M = M_linenr, L = L_linenr }
 end
 
-M.config = {
-  signs = {
-    H = { text = "H", hl_group = "" },
-    M = { text = "M", hl_group = "" },
-    L = { text = "L", hl_group = "" },
-  },
-  priority = 11,
-  enabled = true,
-}
-
 local function create_autocmd()
-
+  vim.api.nvim_create_autocmd({ "CursorMoved", "CursorHold" }, {
+    callback = function()
+      local signs = hml_signs()
+      vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+      for sym, linenr in pairs(signs) do
+        vim.api.nvim_buf_set_extmark(0, ns, linenr - 1, 0, {
+          sign_text = M.config.signs[sym].text,
+          sign_hl_group = "JumpSigns" .. sym,
+          priority = M.config.priority,
+        })
+      end
+    end,
+    desc = "update jumpsigns signs",
+    group = vim.api.nvim_create_augroup("JumpSigns", { clear = true }),
+  })
 end
 
 local function cleanup()
   for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
     vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
   end
-  vim.api.nvim_del_augroup_by_name("jumpsigns")
+  vim.api.nvim_del_augroup_by_name("JumpSigns")
 end
+
 
 function M.setup(opts)
   M.config = vim.tbl_deep_extend("force", M.config, opts or {})
 
-  if M.config.enabled then
-    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorHold" }, {
-      callback = function()
-        vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
-
-        local signs = hml_signs()
-        for sym, linenr in pairs(signs) do
-          vim.api.nvim_buf_set_extmark(vim.api.nvim_get_current_buf(), ns, linenr - 1, 0, {
-            sign_text = M.config.signs[sym].text,
-            sign_hl_group = M.config.signs[sym].hl_group,
-            priority = M.config.priority,
-          })
-        end
-      end,
-      group = vim.api.nvim_create_augroup("jumpsigns", { clear = true }),
-    })
-  else
+  if not M.config.enabled then
     cleanup()
+    return
   end
+
+  -- set up highlight settings
+  for sign, sign_opts in pairs(M.config.signs) do
+    local hl_name = "JumpSigns" .. sign
+    local global = vim.api.nvim_get_hl(0, { name = hl_name }) or {}
+    -- needed bc global is not {} but vim.empty_dict()
+    global = vim.tbl_isempty(global) and nil or global
+    vim.api.nvim_set_hl(
+      0,
+      hl_name,
+      M.config.hl_all or sign_opts.hl_opts or global or { link = "SignColumn" }
+    )
+  end
+
+  create_autocmd()
 end
 
 return M
